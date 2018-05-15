29a4a6123fc354c73cde2efb24698a897db8a0f3
Yuanguo <yuanguo_huo@163.com>
2018-05-15 10:26:16 +0000

    delete some files

--- proxy/metadb_api/metadb-instance-nolcache.lua
+++
@@ -1,8 +0,0 @@
-local ok, metadb = pcall(require, "metadb_api.cache.lcache")
-if not ok or not metadb then
-    error("failed to load metadb_api.cache.lcache:" .. (metadb or "nil"))
-end
-
-local instance = metadb:new(false)
-
-return instance
